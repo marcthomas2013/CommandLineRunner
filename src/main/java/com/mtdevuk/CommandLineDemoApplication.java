46efb5f76ce996b5c40de876a81225aa7a66503b
marc.thomas <marc.thomas@connect-group.com>
2015-07-13 21:39:02 +0100

    Added some contextual help when using properties and overriding them on the command line.

--- src/main/java/com/mtdevuk/CommandLineDemoApplication.java
+++ src/main/java/com/mtdevuk/CommandLineDemoApplication.java
@@ -5,8 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The name property must be defaulted in application.properties, then it can be overridden in an application.properties
+ * file external to your application jar, or passed in via the command line with the parameter --name="Another Name"
+ */
 @SpringBootApplication
 public class CommandLineDemoApplication implements CommandLineRunner {
+
+    // Use @Value to inject the property of the name "name" to nameParameter, this can be done anywhere in your app.
     @Value("${name}")
     private String nameParameter;
 
